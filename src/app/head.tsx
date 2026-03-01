e754ea484ad933af8acd35888a8f43b7135eba27
SushmaUdupaKS <kushisushma64@gmail.com>
2026-03-01 13:19:20 +0530

    Added resume and Power BI download buttons

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Sushma Udupa KS | Portfolio</title>
+      <meta name="description" content="Portfolio website showcasing my work as IAM & Data Analyst" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
